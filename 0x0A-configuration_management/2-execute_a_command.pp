dfef6417136a47ea016a1ad70c3f8df200f431cd
BlackLotusJ <jerrykiche61@gmail.com>
2024-01-22 23:43:49 +0300

    updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,7 @@
-#kills a process named killmenow
-
+# Kills a process name killmenow
 exec { 'pkill killmenow':
-  path => '/usr/bin',
-  command => 'pkill killmenow',
+  path     => '/usr/bin',
+  command  => 'pkill killmenow',
   provider => shell,
-  return => [0, 1]
+  returns  => [0, 1]
 }
