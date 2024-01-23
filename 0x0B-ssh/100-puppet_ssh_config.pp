b6eb27b82fcf32318804e3074c61e8d1d30f07f4
BlackLotusJ <jerrykiche61@gmail.com>
2024-01-23 13:55:34 -0800

    config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#using puppet for configuration
+
+file{'/etc/ssh_config':
+	ensure => present,
+
+content => "
+	
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
